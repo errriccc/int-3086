bf55eac205030f87d6906a8b74471067eaad8056
errriccc <157107933+errriccc@users.noreply.github.com>
2024-12-10 15:51:17 +0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+echo "# int-3086" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/errriccc/int-3086.git
+git push -u origin main
